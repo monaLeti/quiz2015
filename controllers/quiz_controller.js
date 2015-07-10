9b6aaaeb1973cf7ff2232163e17166c1ec065766
leticia.t.v.r@gmail.com <leti@mona.(none)>
2015-07-10 18:46:03 +0200

    Despliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,18 +1,24 @@
+var models = require('../models/models.js');
+
 //GET /quiz/questions
 
 exports.question = function(req, res){
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+	models.Quiz.findAll().success(function(quiz){
+		res.render('quizes/question', {pregunta: quiz[0].pregunta});
+	})
 
 };
 
 //GET /quiz/answers
 
 exports.answer = function(req, res){
-	if(req.query.respuesta == 'Roma'){
-		res.render('quizes/answer', {respuesta: 'Correcta'});
-	}else{
-		res.render('quizes/answer', {respuesta: 'Incorrecta'});
-	}
+	models.Quiz.findAll().success(function(quiz){
+		if(req.query.respuesta === quiz[0].respuesta){
+			res.render('quizes/answer', {respuesta: 'Correcta'});
+		}else{
+			res.render('quizes/answer', {respuesta: 'Incorrecta'});
+		}
+	})
 	
 
 };
