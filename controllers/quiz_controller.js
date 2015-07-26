2a06b09f72c7250505fda31941e72fa0f134ff12
leticia.t.v.r@gmail.com <leti@mona.(none)>
2015-07-26 18:10:46 +0200

    Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -54,6 +54,24 @@ exports.index= function(req, res){
 
 };
 
+//GET /quizes/new
+
+exports.new = function(req, res){
+	var quiz = models.Quiz.build(
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+		);
+	res.render('quizes/new', {quiz: quiz});
+};
+
+//POST /quizes/create
+
+exports.create = function(req, res){
+	var quiz = models.Quiz.build(req.body.quiz);
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+		res.redirect('/quizes');
+	})
+};
+
 //GET /quiz/author
 
 exports.author = function(req, res){
